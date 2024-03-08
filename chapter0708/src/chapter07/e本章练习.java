25bd47539b3d3513be28abaae5a02ead2b94200f
wty <thieisanemail@example.com>
2024-03-08 18:09:24 +0800

    07

--- chapter0708/src/chapter07/e本章练习.java
+++ chapter0708/src/chapter07/e本章练习.java
@@ -4,17 +4,17 @@ package chapter07;
 //1 实现方法max 求double数组的最大值并返回
 class HomeWork01{
     public static void main(String[] args) {
-        double arr[]={};
-        A01 a01=new A01();
-        Double res=a01.max(arr);
+        double[] arr={};
+        DoubleArray doubleArray = new DoubleArray();
+        Double res=doubleArray.findTheMax(arr);
         if(res==null)
             System.out.println("数组不能是null或{}!");
         else
             System.out.println(res);
     }
 }
-class A01{
-    public Double max(double[] arr)//考虑传入数组为空的情况 返回值设为Double Double是一个类
+class DoubleArray{
+    public Double findTheMax(double[] arr)//考虑传入数组为空的情况 返回值设为Double Double是一个类
     {
         if(arr!=null&&arr.length>0)//数组不能是null也不能是{}
         {
@@ -32,6 +32,7 @@ class A01{
     }
 }
 
+
 //2 看图说话 问输出结果
 class Test{
     int count=9;
@@ -46,7 +47,7 @@ class Test{
     }
     //任何一个类都可以有主方法
     public static void main(String[] args) {
-        new Test().count1();//这里new了一个对象但是没有接收的 所以new Test（）叫匿名对象 匿名对象只能使用一次 然后会销毁
+        new Test().count1();//这里new了一个对象但是没有引用接收  new Test()叫匿名对象 匿名对象只能使用一次 然后会销毁
         //然后这个匿名对象调用了count1
         Test t1=new Test();//又new了一个新对象
         t1.count2();
@@ -56,7 +57,6 @@ class Test{
 //总结
 //类与对象定义
 //对象在内存中存在形式
-//成员方法
 //成员方法传参机制
 //方法递归调用
 //作用域
@@ -64,6 +64,3 @@ class Test{
 //可变参数
 //构造器
 //this
-class dog{
-
-}
